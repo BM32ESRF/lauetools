540e50e0b363f468b1d53d58a20a7d204b998052
Jean Sebastien Micha <micha@esrf.fr>
2020-03-31 23:08:30 +0200

    modified again sys.path for automodule

--- doc/conf.py
+++ doc/conf.py
@@ -14,12 +14,9 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 #sys.path.insert(0, os.path.abspath('../..'))
-#sys.path.insert(0, '/home/micha/LaueToolsPy3/LaueTools/Doumentation/source/_static')
-#sys.path.insert(0,'/home/micha/LaueToolsPy3/LaueTools/Doumentation/source/_templates')
-#sys.path.insert(0,'/home/micha/LaueToolsPy3/LaueTools')
 #sys.path.insert(0, os.path.abspath('..'))
 #sys.path.insert(0, os.path.abspath('../..'))
 #sys.path.insert(0,os.path.abspath('../../..'))
