0c9522340bfa406471b4401d82795ca1d6446d83
BM32 ESRF Beamline <115461198+BM32ESRF@users.noreply.github.com>
2024-02-19 16:39:07 +0100

    Delete LaueTools/Daxm/photutils_frozen/background directory

--- LaueTools/Daxm/photutils_frozen/background/__init__.py
+++
@@ -1,8 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""
-This subpackage contains tools to estimate the background and background
-RMS in an image.
-"""
-
-from .background_2d import *  # noqa
-from .core import *  # noqa
