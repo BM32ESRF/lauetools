220b29e420ebbd45b55b468a7c666a4e8bf17e4f
Beatriz Foschiani <beatrizgfoschiani@gmail.com>
2024-04-22 14:31:23 +0200

    Update IOLaueTools.py
    just to add "#" in the last 3 lines (to preserve the original code, not sure if we needed it)

--- LaueTools/IOLaueTools.py
+++ LaueTools/IOLaueTools.py
@@ -2682,7 +2682,7 @@ if __name__ == "__main__":
     res = readCheckOrientationsFile(filepath)
 
 
-    start_func()
-
-    pp = readwriteParametersFile()
-    pp.loadParamsFile('myparams.txt')
+#    start_func()
+#
+#    pp = readwriteParametersFile()
+#    pp.loadParamsFile('myparams.txt')
