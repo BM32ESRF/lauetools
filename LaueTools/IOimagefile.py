c8f513caaf2c743f08e0534e846f700f3d4ceb35
BM32ESRF <115461198+BM32ESRF@users.noreply.github.com>
2024-05-06 11:28:46 +0200

    open EIGER 4M with fabio module (easier to install)

--- LaueTools/IOimagefile.py
+++ LaueTools/IOimagefile.py
@@ -786,7 +786,7 @@ def readCCDimage(filename, CCDLabel="MARCCD165", dirname=None, stackimageindex=-
         print('PIL_EXISTS',PIL_EXISTS)
     #    if extension != extension:
     #        print "warning : file extension does not match CCD type set in Set CCD File Parameters"
-    if FABIO_EXISTS and CCDLabel not in ('Alban','psl_IN_bmp','EIGER_4M'):
+    if FABIO_EXISTS and CCDLabel not in ('Alban','psl_IN_bmp'):#,'EIGER_4M'):
         if CCDLabel in ('MARCCD165', "EDF", "EIGER_4M", "EIGER_1M","IMSTAR_bin2","IMSTAR_bin1","RXO",
                         "sCMOS", "sCMOS_fliplr", "sCMOS_fliplr_16M", "sCMOS_16M",
                         "Rayonix MX170-HS", 'psl_weiwei', 'ImageStar_dia_2021',
