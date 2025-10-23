20cfba84e96889e11a7d1a6b9328084d347ed07c
jsmicha <57399525+jsmicha@users.noreply.github.com>
2025-10-23 16:52:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ if sys.version_info.major == 3:
 	'plotmap = LaueTools.FileSeries.Plot_Maps2:start',
 	'mapanalyzer = LaueTools.FileSeries.mainFileSeriesGUI:start',
 	'daxmgui = LaueTools.daxmgui:start',
-	'lauetools-copy=lauetools.cli:copy_resources',
-	'lauetools-copy-materials=lauetools.cli:copy_materials']}
+	'lauetools-copy=Lauetools.cli:copy_resources',
+	'lauetools-copy-materials=Lauetools.cli:copy_materials']}
 else:
     python3entry_points= {}
 
