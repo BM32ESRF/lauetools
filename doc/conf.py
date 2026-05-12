43a27d7b556dee9227f24494de1ef21a1332de1f
jsmicha <57399525+jsmicha@users.noreply.github.com>
2026-05-12 16:44:48 +0200

    Update intersphinx mapping for Python documentation

--- doc/conf.py
+++ doc/conf.py
@@ -210,4 +210,6 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': ('https://docs.python.org/objects.inv', None)}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+}
